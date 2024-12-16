0e0e2fca64514ac6f3f8f26c746c6a4cf01226fd
HusseinSerag <husseinserag2014@gmail.com>
2024-12-16 14:10:43 +0100

    fix: remove build from repo

--- build/routes/ai.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const account_controller_1 = require("../controllers/account.controller");
-const requireAuth_1 = require("../middleware/requireAuth");
-const router = (0, express_1.Router)();
-router.post("/generate", requireAuth_1.requireAuth, account_controller_1.generateAIemail);
-exports.default = router;
--- build/routes/email.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const email_controller_1 = require("../controllers/email.controller");
-const requireAuth_1 = require("../middleware/requireAuth");
-const router = (0, express_1.Router)();
-router.get("/auth/url", requireAuth_1.requireAuth, email_controller_1.getAurinkoUrl);
-router.get("/callback", email_controller_1.onboardEmail);
-exports.default = router;
